610a1a59aff767aadea07c7000f74a0dbde44197
Peter Hahndorf <peterhahndorf@gmail.com>
2015-02-16 16:46:00 +0100

    Added initial version
    Version 1

---
+++ DownloadAlerter/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DownloadAlerter")]
+[assembly: AssemblyDescription("IIS Url Rewrite Custom Provider")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter Hahndorf")]
+[assembly: AssemblyProduct("DownloadAlerter")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DownloadAlerter/DownloadAlerter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using Microsoft.Web.Iis.Rewrite;
+
+public class DownloadAlerter : IRewriteProvider, IProviderDescriptor
+{
+    private string recipient, sender, server;
+
+    public void Initialize(IDictionary<string, string> settings, IRewriteContext rewriteContext)
+    {
+        if (!settings.TryGetValue("Recipient", out recipient) || string.IsNullOrEmpty(recipient))
+            throw new ArgumentException("Recipient provider setting is required and cannot be empty");
+
+        if (!settings.TryGetValue("Sender", out sender) || string.IsNullOrEmpty(sender))
+            throw new ArgumentException("Sender provider setting is required and cannot be empty");
+
+        if (!settings.TryGetValue("Server", out server) || string.IsNullOrEmpty(server))
+            throw new ArgumentException("Server provider setting is required and cannot be empty");
+    }
+
+    public string Rewrite(string value)
+    {
+        MailMessage message = new MailMessage();
+        message.From = new MailAddress(sender);
+
+        message.To.Add(new MailAddress(recipient));
+
+        message.Subject = "Download Alert";
+        message.Body = string.Format("The following URL was downloaded: '{0}'" + Environment.NewLine + "For details check your IIS logs", value);
+
+        SmtpClient client = new SmtpClient();
+        client.Host = server;
+        client.Send(message);
+
+        return value;
+    }
+
+    public IEnumerable<SettingDescriptor> GetSettings()
+    {
+        yield return new SettingDescriptor("Recipient", "Email address of the recipient");
+        yield return new SettingDescriptor("Sender", "Email address of the sender");
+        yield return new SettingDescriptor("Server", "The SMTP server to be used");
+    }
+}
